891335800a7ddb289bda5f22963835e89742c5f5
dbpeetz <d.peetz516@gmail.com>
2017-04-17 15:16:01 -0500

    Added Customer Model and Product Model

---
+++ KnotYourAverageTies/Models/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnotYourAverageTies.Models
+{
+    public class Customers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
---
+++ KnotYourAverageTies/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KnotYourAverageTies.Models
+{
+    public class Products
+    {
+        [Key]
+        public int UPC { get; set; }
+
+        [Display(Name="Name")]
+        public string Name { get; set; }
+        public string Section { get; set; }
+        public double Price { get; set; }
+        public int Inventory { get; set; }
+
+
+
+    }
+}
